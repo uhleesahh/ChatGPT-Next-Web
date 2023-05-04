d0d04be1ea41c54da2e2a3b33a7b806371d99930
uhleesahh <132541894+uhleesahh@users.noreply.github.com>
2023-05-04 11:09:04 -0700

    Update index.ts

--- app/masks/index.ts
+++ app/masks/index.ts
@@ -20,6 +20,6 @@ export const BUILTIN_MASK_STORE = {
   },
 };
 
-export const BUILTIN_MASKS: Mask[] = [...CN_MASKS, ...EN_MASKS].map((m) =>
+export const BUILTIN_MASKS: Mask[] = [...EN_MASKS].map((m) =>
   BUILTIN_MASK_STORE.add(m),
 );
