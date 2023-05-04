5710bd8d79807b09b553c823267c11ec31ec47c0
uhleesahh <132541894+uhleesahh@users.noreply.github.com>
2023-05-04 11:16:22 -0700

    Update fetch-prompts.mjs

--- scripts/fetch-prompts.mjs
+++ scripts/fetch-prompts.mjs
@@ -4,8 +4,6 @@ import fs from "fs/promises";
 const RAW_FILE_URL = "https://raw.githubusercontent.com/";
 const MIRRORF_FILE_URL = "https://raw.fgit.ml/";
 
-const RAW_CN_URL = "PlexPt/awesome-chatgpt-prompts-zh/main/prompts-zh.json";
-const CN_URL = MIRRORF_FILE_URL + RAW_CN_URL;
 const RAW_EN_URL = "f/awesome-chatgpt-prompts/main/prompts.csv";
 const EN_URL = MIRRORF_FILE_URL + RAW_EN_URL;
 const FILE = "./public/prompts.json";
@@ -17,19 +15,6 @@ const timeoutPromise = (timeout) => {
     }, timeout);
   });
 };
-
-async function fetchCN() {
-  console.log("[Fetch] fetching cn prompts...");
-  try {
-    // const raw = await (await fetch(CN_URL)).json();
-    const response = await Promise.race([fetch(CN_URL), timeoutPromise(5000)]);
-    const raw = await response.json();
-    return raw.map((v) => [v.act, v.prompt]);
-  } catch (error) {
-    console.error("[Fetch] failed to fetch cn prompts", error);
-    return [];
-  }
-}
 
 async function fetchEN() {
   console.log("[Fetch] fetching en prompts...");
@@ -48,13 +33,13 @@ async function fetchEN() {
 }
 
 async function main() {
-  Promise.all([fetchCN(), fetchEN()])
-    .then(([cn, en]) => {
-      fs.writeFile(FILE, JSON.stringify({ cn, en }));
+  Promise.all(fetchEN()])
+    .then(([en]) => {
+      fs.writeFile(FILE, JSON.stringify({ en }));
     })
     .catch((e) => {
       console.error("[Fetch] failed to fetch prompts");
-      fs.writeFile(FILE, JSON.stringify({ cn: [], en: [] }));
+      fs.writeFile(FILE, JSON.stringify({ en: [] }));
     })
     .finally(() => {
       console.log("[Fetch] saved to " + FILE);
